971661c535b135300ddec5bf8dcc97af0653a157
Quach Cong Tuan <84640308+Tunniez@users.noreply.github.com>
2022-01-19 23:40:53 +0700

    Update Connect
    Change Username && Password Database

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
     $severname = "localhost";
     $username = "id17446086_tunesource";
-    $password = "-m@_<cs64nbQif+<";
-    $database = "id17446086_music";
+    $password = "-m@_<cs64nbQif+<---testgithub";
+    $database = "id17446086_music_testgithub";
     //Khai báo biến để kết nỗi CSDL
     $connect = new mysqli($severname,$username,$password,$database);
     //$connect = new mysqli_connect ----> Hướng Thủ Tục
